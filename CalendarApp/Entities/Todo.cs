44d966779bb5d44fece443fdf6fd3dc49491f9c7
Savcdilara <108372749+Savcdilara@users.noreply.github.com>
2023-09-25 16:52:30 +0000

    file push

---
+++ CalendarApp/Entities/Todo.cs
@@ -0,0 +1,16 @@
+﻿using CalendarApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApp.Entities
+{
+    internal class Todo : Event
+    {
+        public string Importance { get; set; }
+        
+
+    }
+}
---
+++ CalendarApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using CalendarApp.Entities;
+
+Console.WriteLine("Calendar App");
+
+Meeting meeting1 = new()
+{
+    Title = "YetGen Jump & Akbank Backend Planlama Toplantısı",
+    Details =new List<string>()
+    { 
+        "Katılımcıların alım süreçleri konuşulacak", 
+        "Akbank'tan gelen mail konuşulacak" 
+    },
+    StartTime= new DateTime(2023, 09, 18, 19, 00, 00 ),
+    FinishTime= new DateTime(2023, 09, 18, 19, 00, 00 ),
+    Guests = new() { "hakan@deneme.com", "mehmet@deneme.com"},
+};
+Todo todo1 = new()
+{
+    Title = "Katılımcı geri bildirimlerini teslim et",
+    Details = new List<string>()
+    {
+        "Değerlendirme sonuçlarının anonimleştirilmesi",
+        "grafik oluşturulması",
+    },
+    StartTime = new DateTime(2023, 09, 20, 21, 00, 00),
+    FinishTime = new DateTime(2023, 09, 20, 20, 00, 00),
+    Importance = "High Priority",
+
+};
+
+
+
